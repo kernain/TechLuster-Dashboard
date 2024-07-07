6a2a85daff513b1d58364e0e5259a283d639c352
syed kernain <hasansyed700@gmail.com>
2024-07-08 01:43:39 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import SidebarMenu from './components/SidebarMenu';
-import "./App.css"
+import ReactDOM from 'react-dom/client';
 import { Box } from '@mui/material';
+import SidebarMenu from './components/SidebarMenu';
 import Dashboard from './screens/Dashboard/Dashboard';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme/theme';
+import './App.css';
 
 const App = () => {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Box className="sidebar">
-        <SidebarMenu />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', height: '100vh' }}>
+        <Box sx={{ width: '100px' }}>
+          <SidebarMenu />
+        </Box>
+        <Box sx={{ flexGrow: 1, overflow: 'auto' }}>
+          <Dashboard />
+        </Box>
       </Box>
-      <Box className="main" >
-        <Dashboard/>
-      </Box>
-    </Box>
+    </ThemeProvider>
   );
 };
 
